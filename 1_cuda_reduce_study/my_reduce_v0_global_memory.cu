8648225c1ab4cf27635ff086209303ba385a7317
momo <569377793@qq.com>
2026-02-25 19:07:08 +0800

    基础框架

--- 1_cuda_reduce_study/my_reduce_v0_global_memory.cu
+++ 1_cuda_reduce_study/my_reduce_v0_global_memory.cu
@@ -3,8 +3,75 @@
 #include<stdlib.h>
 #include<cuda_runtime.h>
 
+#define THREAD_PER_BLOCK 256
+
+__global__ void reduce(float *d_in, float *d_out)
+{
+
+}
+
+bool check(float *out, float *res, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if(abs(out[i] - res[i]) > 1e-5)
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
-    printf("hello reduce");
+    // printf("hello reduce");
+    const int N = 32 * 1024 * 1024;
+    float *input = (float *)malloc(N * sizeof(float));
+    float *d_input;
+    cudaMalloc((void **)&d_input, N * sizeof(float));
+
+    int block_num = N / THREAD_PER_BLOCK;
+    float *output = (float *)malloc(block_num * sizeof(float));
+    float *d_output;
+    cudaMalloc((void **)&d_output, block_num * sizeof(float));
+
+    float *result = (float *)malloc(block_num * sizeof(float));
+    for(int i = 0; i < N; i++)
+    {
+        input[i] = 2.0 * (float)drand48() - 1.0;
+    }
+
+    // cpu calculate
+    for(int i = 0; i < block_num; i++)
+    {
+        float cur = 0;
+        for(int j = 0; j < THREAD_PER_BLOCK; j++)
+        {
+            cur += input[i * THREAD_PER_BLOCK + j];
+        }
+        result[i] = cur;
+    }
+
+    cudaMemcpy(d_input, input, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 Grid(block_num, 1);
+    dim3 Block(THREAD_PER_BLOCK, 1);
+
+    reduce<<<Grid, Block>>>(d_input, d_output);
+
+    cudaMemcpy(output, d_output, block_num * sizeof(float), cudaMemcpyDeviceToHost);
+    if(check(output, result, block_num))
+        printf("the ans is right\n");
+    else
+    {
+        printf("the ans is wrong\n");
+        for(int i = 0; i < block_num; i++)
+        {
+            printf("%lf", output[i]);
+        }
+        printf("\n");
+    }
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+
     return 0;
 }
