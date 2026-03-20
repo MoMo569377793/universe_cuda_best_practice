82b2e1eaf22366445d3538bd25e448cde0f10e3c
momo <569377793@qq.com>
2026-03-20 17:14:49 +0800

    finsh sgemm v7

--- 2_cuda_sgemm_study/my_sgemm_v7_double_buffer.cu
+++ 2_cuda_sgemm_study/my_sgemm_v7_double_buffer.cu
@@ -55,12 +55,12 @@ float compare_matrices(int M, int N, float *a, float *b)
 
 #define FETCH_FLOAT4(val) (reinterpret_cast<float4 *>(&(val))[0])
 
-template <unsigned int M_NUM_PER_BLOCK,
-          unsigned int N_NUM_PER_BLOCK,
-          unsigned int K_NUM_PER_BLOCK,
-          unsigned int M_NUM_PER_THREAD,
-          unsigned int N_NUM_PER_THREAD,
-          unsigned int K_NUM_PER_THREAD>
+template <unsigned int BLOCK_SIZE_M,    // height of block of C that each block calculate
+          unsigned int BLOCK_SIZE_K,    // width  of block of A that each block load into shared memory
+          unsigned int BLOCK_SIZE_N,    // width  of block of C that each block calculate
+          unsigned int THREAD_SIZE_Y,   // height of block of C that each thread calculate
+          unsigned int THREAD_SIZE_X,   // width  of block of C that each thread calculate
+          const bool ENABLE_DOUBLE_BUFFER>  // whether enable double buffering or not
 __global__ void cuda_sgemm(float *A_ptr, float *B_ptr, float *C_ptr, const int M, const int N, const int K)
 {
     // Block index
@@ -74,52 +74,87 @@ __global__ void cuda_sgemm(float *A_ptr, float *B_ptr, float *C_ptr, const int M
     // thread id in cur Block
     const int tid = ty * blockDim.x + tx;
 
-    __shared__ float a_shared[2][M_NUM_PER_BLOCK][K_NUM_PER_BLOCK];
-    __shared__ float b_shared[2][K_NUM_PER_BLOCK][N_NUM_PER_BLOCK];
+    __shared__ float a_shared[2][BLOCK_SIZE_K][BLOCK_SIZE_M];
+    __shared__ float b_shared[2][BLOCK_SIZE_K][BLOCK_SIZE_N];
+
+    float accum[THREAD_SIZE_Y][THREAD_SIZE_X] = {0.f};
+    float reg_a[THREAD_SIZE_Y] = {0.f};
+    float reg_b[THREAD_SIZE_X] = {0.f};
+    float ldg_a_reg[4] = {0.f};
     
-    float *A_ptr_start = A_ptr + blockIdx.y * M_NUM_PER_BLOCK * K;
-    float *B_ptr_start = B_ptr + blockIdx.x * N_NUM_PER_BLOCK;
+    float *A_ptr_start = A_ptr + blockIdx.y * BLOCK_SIZE_M * K;
+    float *B_ptr_start = B_ptr + blockIdx.x * BLOCK_SIZE_N;
 
+    // block内部索引重排
+    const int A_tile_thread_per_row = BLOCK_SIZE_K / 4; // 2
+    const int B_tile_thread_per_row = BLOCK_SIZE_N / 4; // 32
+    
+    const int A_tile_tid_x = tid % A_tile_thread_per_row;
+    const int A_tile_tid_y = tid / A_tile_thread_per_row;
+    const int B_tile_tid_x = tid % B_tile_thread_per_row;
+    const int B_tile_tid_y = tid / B_tile_thread_per_row;
 
+    // 流水线的装载和排空阶段不是满流水的，不能放在循环里
+    FETCH_FLOAT4(ldg_a_reg[0]) = FETCH_FLOAT4(A_ptr_start[K * A_tile_tid_y + A_tile_tid_x * 4]);
+    // 当前线程在block中的横坐标 * 每个线程横向处理几个数据(4)
+    // 当前线程在block中的纵坐标 * 每个线程纵向处理几个数据(1)
+    a_shared[0][A_tile_tid_x * 4][A_tile_tid_y] = ldg_a_reg[0];
+    a_shared[0][A_tile_tid_x * 4 + 1][A_tile_tid_y] = ldg_a_reg[1];
+    a_shared[0][A_tile_tid_x * 4 + 2][A_tile_tid_y] = ldg_a_reg[2];
+    a_shared[0][A_tile_tid_x * 4 + 3][A_tile_tid_y] = ldg_a_reg[3];
+    FETCH_FLOAT4(b_shared[0][B_tile_tid_y][B_tile_tid_x * 4]) = FETCH_FLOAT4(B_ptr_start[N * B_tile_tid_y + B_tile_tid_x * 4]);
+    __syncthreads();
 
-    float a_reg[M_NUM_PER_THREAD] = {0.f};
-    float b_reg[N_NUM_PER_THREAD] = {0.f};
-    float a_load_reg[K_NUM_PER_THREAD] = {0.f};
-    float temp[M_NUM_PER_THREAD][N_NUM_PER_THREAD] = {0.f};
-
-    for(int s = 0; s < K; s += K_NUM_PER_BLOCK)
+    int write_stage_idx = 1;
+    for(int s = BLOCK_SIZE_K; s < K; s += BLOCK_SIZE_K)
     {
-        for(int i = 0; i < M_NUM_PER_THREAD; i++)
-        {
-            FETCH_FLOAT4(a_load_reg[0]) = FETCH_FLOAT4(A_ptr_start[(ty * M_NUM_PER_THREAD + i) * K + tx * K_NUM_PER_THREAD + s]);
-            a_shared[tx * K_NUM_PER_THREAD][ty * M_NUM_PER_THREAD + i] = a_load_reg[0];
-            a_shared[tx * K_NUM_PER_THREAD + 1][ty * M_NUM_PER_THREAD + i] = a_load_reg[1];
-            a_shared[tx * K_NUM_PER_THREAD + 2][ty * M_NUM_PER_THREAD + i] = a_load_reg[2];
-            a_shared[tx * K_NUM_PER_THREAD + 3][ty * M_NUM_PER_THREAD + i] = a_load_reg[3];
-        }
-
-        for(int i = 0; i < N_NUM_PER_THREAD; i++)
-        {
-            FETCH_FLOAT4(b_shared[ty * K_NUM_PER_THREAD + i][tx * N_NUM_PER_THREAD]) = FETCH_FLOAT4(B_ptr_start[N * (ty * K_NUM_PER_THREAD + s + i) + tx * N_NUM_PER_THREAD]);        }
-
-        __syncthreads();
-
-        for(int k = 0; k < K_NUM_PER_BLOCK; k++)
-        {
-            FETCH_FLOAT4(a_reg[0]) = FETCH_FLOAT4(a_shared[k][ty * M_NUM_PER_THREAD]);
-            FETCH_FLOAT4(b_reg[0]) = FETCH_FLOAT4(b_shared[k][tx * N_NUM_PER_THREAD]);
+        FETCH_FLOAT4(ldg_a_reg[0]) = FETCH_FLOAT4(A_ptr_start[K * A_tile_tid_y + A_tile_tid_x * 4 + s]);
+        a_shared[write_stage_idx][A_tile_tid_x * 4][A_tile_tid_y] = ldg_a_reg[0];
+        a_shared[write_stage_idx][A_tile_tid_x * 4 + 1][A_tile_tid_y] = ldg_a_reg[1];
+        a_shared[write_stage_idx][A_tile_tid_x * 4 + 2][A_tile_tid_y] = ldg_a_reg[2];
+        a_shared[write_stage_idx][A_tile_tid_x * 4 + 3][A_tile_tid_y] = ldg_a_reg[3];
+        FETCH_FLOAT4(b_shared[write_stage_idx][B_tile_tid_y][B_tile_tid_x * 4]) = FETCH_FLOAT4(B_ptr_start[N * (B_tile_tid_y + s) + B_tile_tid_x * 4]);
         
-            for(int i = 0; i < M_NUM_PER_THREAD; i++)
-            for(int j = 0; j < N_NUM_PER_THREAD; j++)
-                temp[i][j] += a_reg[i] * b_reg[j];
+
+        write_stage_idx ^= 1;
+        for(int k = 0; k < BLOCK_SIZE_K; k++)
+        {
+            // 从共享内存搬运到寄存器中服务下一步计算
+            FETCH_FLOAT4(reg_a[0]) = FETCH_FLOAT4(a_shared[write_stage_idx][k][ty * THREAD_SIZE_Y]);
+            FETCH_FLOAT4(reg_a[4]) = FETCH_FLOAT4(a_shared[write_stage_idx][k][ty * THREAD_SIZE_Y + 4]);
+
+            FETCH_FLOAT4(reg_b[0]) = FETCH_FLOAT4(b_shared[write_stage_idx][k][tx * THREAD_SIZE_X]);
+            FETCH_FLOAT4(reg_b[4]) = FETCH_FLOAT4(b_shared[write_stage_idx][k][tx * THREAD_SIZE_X + 4]);
+        
+            for(int i = 0; i < THREAD_SIZE_Y; i++)
+                for(int j = 0; j < THREAD_SIZE_X; j++)
+                    accum[i][j] += reg_a[i] * reg_b[j];
         }
         __syncthreads();
+
+    }
+    write_stage_idx = write_stage_idx ^ 1;
+    for(int k = 0; k < BLOCK_SIZE_K; k++)
+    {
+        // 从共享内存搬运到寄存器中服务下一步计算
+        FETCH_FLOAT4(reg_a[0]) = FETCH_FLOAT4(a_shared[write_stage_idx][k][ty * THREAD_SIZE_Y]);
+        FETCH_FLOAT4(reg_a[4]) = FETCH_FLOAT4(a_shared[write_stage_idx][k][ty * THREAD_SIZE_Y + 4]);
+
+        FETCH_FLOAT4(reg_b[0]) = FETCH_FLOAT4(b_shared[write_stage_idx][k][tx * THREAD_SIZE_X]);
+        FETCH_FLOAT4(reg_b[4]) = FETCH_FLOAT4(b_shared[write_stage_idx][k][tx * THREAD_SIZE_X + 4]);
+    
+        for(int i = 0; i < THREAD_SIZE_Y; i++)
+            for(int j = 0; j < THREAD_SIZE_X; j++)
+                accum[i][j] += reg_a[i] * reg_b[j];
     }
 
-    float *C_ptr_start = C_ptr + N * blockIdx.y * N_NUM_PER_BLOCK + 
-                         blockIdx.x * N_NUM_PER_BLOCK;
-    for(int i = 0; i < N_NUM_PER_THREAD; i++)
-        FETCH_FLOAT4(C_ptr_start[N * (ty * M_NUM_PER_THREAD + i) + tx * N_NUM_PER_THREAD]) = FETCH_FLOAT4(temp[i][0]);
+    float *C_ptr_start = C_ptr + N * by * BLOCK_SIZE_M + 
+                         bx * BLOCK_SIZE_N;
+    for(int i = 0; i < THREAD_SIZE_Y; i++)
+    {
+        FETCH_FLOAT4(C_ptr_start[N * (ty * THREAD_SIZE_Y + i) + tx * THREAD_SIZE_X]) = FETCH_FLOAT4(accum[i][0]);
+        FETCH_FLOAT4(C_ptr_start[N * (ty * THREAD_SIZE_Y + i) + tx * THREAD_SIZE_X + 4]) = FETCH_FLOAT4(accum[i][4]);
+    }
 }
 
 int main()
@@ -152,18 +187,19 @@ int main()
 
     cpu_sgemm(matrix_A_host, matrix_B_host, matrix_C_cpu_calc, m, n, k);
 
-    constexpr int M_NUM_PER_BLOCK = 64;
-    constexpr int N_NUM_PER_BLOCK = 64;
-    constexpr int K_NUM_PER_BLOCK = 64;
-    // constexpr int NUM_PER_THREAD = 16;
-    constexpr int M_NUM_PER_THREAD = 4;
-    constexpr int N_NUM_PER_THREAD = 4;
-    constexpr int K_NUM_PER_THREAD = 4;
+    constexpr int BLOCK_SIZE_M = 128;
+    constexpr int BLOCK_SIZE_K = 8;
+    constexpr int BLOCK_SIZE_N = 128;
 
-    dim3 block(16, 16);
-    dim3 grid(n / N_NUM_PER_BLOCK, m / M_NUM_PER_BLOCK);
+    constexpr int THREAD_SIZE_Y = 8;
+    constexpr int THREAD_SIZE_X = 8;
+    const bool ENABLE_DOUBLE_BUFFER = true;
 
-    cuda_sgemm<M_NUM_PER_BLOCK, N_NUM_PER_BLOCK, K_NUM_PER_BLOCK, M_NUM_PER_THREAD, N_NUM_PER_THREAD, K_NUM_PER_THREAD><<<grid, block>>>(matrix_A_device, matrix_B_device, matrix_C_device, m, n, k);
+    // 等同于dim3 block(16, 16);
+    dim3 block(BLOCK_SIZE_N / THREAD_SIZE_X, BLOCK_SIZE_M / THREAD_SIZE_Y);
+    dim3 grid(n / BLOCK_SIZE_N, m / BLOCK_SIZE_M);
+
+    cuda_sgemm<BLOCK_SIZE_M, BLOCK_SIZE_K, BLOCK_SIZE_N, THREAD_SIZE_Y, THREAD_SIZE_X, ENABLE_DOUBLE_BUFFER><<<grid, block>>>(matrix_A_device, matrix_B_device, matrix_C_device, m, n, k);
 
     cudaMemcpy(matrix_C_gpu_calc, matrix_C_device, mem_size_C, cudaMemcpyDeviceToHost);
 
